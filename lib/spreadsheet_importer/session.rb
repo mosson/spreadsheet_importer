92bdafd5de66383321b027126ffd7df372bb549f
mosson <cucation@gmail.com>
2015-08-18 21:36:15 +0900

    encodingを追加

--- lib/spreadsheet_importer/session.rb
+++ lib/spreadsheet_importer/session.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module SpreadsheetImporter
   module Session
     module ModuleMethods
--- lib/spreadsheet_importer/spreadsheet.rb
+++ lib/spreadsheet_importer/spreadsheet.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module SpreadsheetImporter
   class Spreadsheet
     attr_accessor :key, :title
